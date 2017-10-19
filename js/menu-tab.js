9bc071ea90c5bf7d26af3e5ce2d9086f57fc178e
fliflifli <fliflifli@naver.com>
2017-10-19 16:27:36 +0900

    10월 19일

---
+++ js/menu-tab.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  var menu = $('.main-menu > li');
+  var item = $('.main-menu span')
+  var subMenu = $('.sub-menu');
+  var img = $('.main-menu img');
+
+  menu.hover(function() {
+    $(this).find(subMenu).toggleClass('sub-menu-act');
+  });
+  menu.focusin(function() {
+    $(this).siblings().find(subMenu).removeClass('sub-menu-act');
+    $(this).find(subMenu).addClass('sub-menu-act');
+  });
+
+  menu.mouseover(function() {
+      var src = $(this).find(img).attr('src').match(/[^\.]+/) + '_ov.gif';
+      $(this).find(img).attr('src', src);
+    })
+    .mouseout(function() {
+      var src = $(this).find(img).attr('src').replace('_ov.gif', '.gif');
+      $(this).find(img).attr('src', src);
+
+    });
+  menu.focusin(function() {
+      var src = $(this).find(img).attr('src').match(/[^\.]+/) + '_ov.gif';
+      $(this).find(img).attr('src', src);
+    })
+    .focusout(function() {
+      var src = $(this).find(img).attr('src').replace('_ov.gif', '.gif');
+      $(this).find(img).attr('src', src);
+    });
+
+});
